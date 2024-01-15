d7a80ec4d95c4545f700743b95e7fc3f44cceac7
Daniel Oliveira <oliveira.d.c@outlook.com>
2024-01-15 11:22:29 -0300

    adicionar opção /renew

--- setup.cmd
+++ setup.cmd
@@ -56,6 +56,14 @@ if not exist "%~dp0config\config.txt" (
 )
 for /F "usebackq tokens=*" %%V in ( `type "%~dp0config\config.txt" ^| findstr /V "^::"` ) do ( set %%V )
 
+:: deletar usuario antigo e criar novo
+if "%1" == "/renew" (
+	net user %usuario_padrao% /delete
+	net user %usuario_padrão% /add
+	net localgroup Administradores %usuario_padrao% /add
+	shutdown /l
+)
+
 :: Verificação do nome do computador
 ver > nul
 if %renomear_maquina%==sempre (
